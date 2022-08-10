b06daf9836a700974304bd7b7dffd4da8c7895ec
Rayhan <prog.rayhan1@gmail.com>
2022-08-10 16:49:33 +0600

    Creating JSON Response - Individual Elements

--- watchmate/watchlist_app/urls.py
+++ watchmate/watchlist_app/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import movie_list
+from .views import movie_details, movie_list
 
 urlpatterns = [
     path('list/', movie_list, name="movie-list"),
+    path('<int:pk>', movie_details, name="movie-details"),
 ]
