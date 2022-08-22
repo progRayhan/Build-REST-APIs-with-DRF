313e4dd3aaead935f1869064a1cab878e37feafc
Rayhan <prog.rayhan1@gmail.com>
2022-08-22 13:22:17 +0600

    APIView class added

--- watchmate/watchlist_app/api/urls.py
+++ watchmate/watchlist_app/api/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from watchlist_app.api.views import movie_details, movie_list
+# from watchlist_app.api.views import movie_details, movie_list
+from watchlist_app.api.views import MovieListAV, MovieDetailAV
 
 urlpatterns = [
-    path('list/', movie_list, name="movie-list"),
-    path('<int:pk>', movie_details, name="movie-details"),
+    path('list/', MovieListAV.as_view(), name="movie-list"),
+    path('<int:pk>', MovieDetailAV.as_view(), name="movie-details"),
 ]
