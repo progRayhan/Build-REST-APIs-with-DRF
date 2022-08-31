42cf963347ad0c4463a1ed60ab5ad645ff9d7ded
Rayhan <prog.rayhan1@gmail.com>
2022-08-31 14:02:17 +0600

    serializer relations

--- watchmate/watchlist_app/models.py
+++ watchmate/watchlist_app/models.py
@@ -1,5 +1,6 @@
 from venv import create
 from django.db import models
+from django.core.validators import MinValueValidator,MaxValueValidator
 
 # Create your models here.
 class StreamPlatform(models.Model):
@@ -19,6 +20,18 @@ class WatchList(models.Model):
     def __str__(self):
         return self.title
     
+
+class Review(models.Model):
+    rating = models.PositiveIntegerField(validators=[MinValueValidator(1),MaxValueValidator(10)])
+    description = models.CharField(max_length=200, null=True)
+    watchlist = models.ForeignKey(WatchList, on_delete=models.CASCADE, related_name="reviews")
+    active = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return str(self.rating) + ' | ' + self.watchlist.title
+        # return str(self.rating)
     
     
     
