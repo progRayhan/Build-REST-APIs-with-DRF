94bf8c4dc370be225c79b04b472d21e55d5fc0e7
Rayhan <prog.rayhan1@gmail.com>
2022-09-03 13:35:51 +0600

    added review url structure

--- watchmate/watchlist_app/api/urls.py
+++ watchmate/watchlist_app/api/urls.py
@@ -10,6 +10,9 @@ urlpatterns = [
     path('stream/', StreamPlatformAV.as_view(), name="stream-platform"),
     path('stream/<int:pk>/', StreamPlatformDetailsAV.as_view(), name="streamplatform-detail"),
     
-    path('review/', ReviewList.as_view(), name="review-list"),
-    path('review/<int:pk>/', ReviewDetail.as_view(), name="review-detail"),
+    # path('review/', ReviewList.as_view(), name="review-list"),
+    # path('review/<int:pk>/', ReviewDetail.as_view(), name="review-detail"),
+    
+    path('stream/<int:pk>/review', StreamPlatformDetailsAV.as_view(), name="streamplatform-detail"),
+    path('stream/review/<int:pk>', ReviewDetail.as_view(), name="review-detail"),
 ]
