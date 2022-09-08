05663b53a67649133ccc8051d52cc3d3095a1725
Rayhan <prog.rayhan1@gmail.com>
2022-09-08 09:55:36 +0600

    Overwrite Queryset added

--- watchmate/watchlist_app/api/serializers.py
+++ watchmate/watchlist_app/api/serializers.py
@@ -6,7 +6,8 @@ from watchlist_app.models import WatchList, StreamPlatform, Review
 class ReviewSerializer(serializers.ModelSerializer):
     class Meta:
         model = Review
-        fields = "__all__"
+        # fields = "__all__"
+        exclude = ('watchlist',)
 class WatchListSerializer(serializers.ModelSerializer):
     reviews = ReviewSerializer(many=True, read_only=True)
     class Meta:
--- watchmate/watchlist_app/api/urls.py
+++ watchmate/watchlist_app/api/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 # from watchlist_app.api.views import movie_details, movie_list
 # from watchlist_app.api.views import MovieListAV, MovieDetailAV
 from watchlist_app.api.views import (ReviewDetail, StreamPlatformAV, StreamPlatformDetailsAV, 
-                                     WatchListAV, WatchListDetailAV, ReviewList)
+                                     WatchListAV, WatchListDetailAV, ReviewList, ReviewCreate)
 
 urlpatterns = [
     path('list/', WatchListAV.as_view(), name="watch-list"),
@@ -11,9 +11,10 @@ urlpatterns = [
     path('stream/', StreamPlatformAV.as_view(), name="stream-platform"),
     path('stream/<int:pk>/', StreamPlatformDetailsAV.as_view(), name="streamplatform-detail"),
     
-    path('review/', ReviewList.as_view(), name="review-list"),
-    path('review/<int:pk>/', ReviewDetail.as_view(), name="review-detail"),
+    # path('review/', ReviewList.as_view(), name="review-list"),
+    # path('review/<int:pk>/', ReviewDetail.as_view(), name="review-detail"),
     
-    # path('stream/<int:pk>/review', StreamPlatformDetailsAV.as_view(), name="streamplatform-detail"),
-    # path('stream/review/<int:pk>', ReviewDetail.as_view(), name="review-detail"),
+    path('stream/<int:pk>/review-create', ReviewCreate.as_view(), name="review-create"),
+    path('stream/<int:pk>/review', ReviewList.as_view(), name="review-list"),
+    path('stream/review/<int:pk>', ReviewDetail.as_view(), name="review-detail"),
 ]
